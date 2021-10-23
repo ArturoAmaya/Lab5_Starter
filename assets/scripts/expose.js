cf5abc6c66e6e6eea7ad8b2305df92afa7cb8c1b
ArturoAmaya <a1amaya@ucsd.edu>
2021-10-22 18:59:23 -0700

    first commit, got image to change

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,24 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+
+  // Horn selector
+  /*When you select a horn from the drop down menu, the following should occur:
+  The correct image should display
+  The correct audio sound file should be set*/
+  const hornSelect = document.getElementById("horn-select")
+  const images = document.querySelector('img');
+  console.log(images); 
+
+  hornSelect.addEventListener('input', updateHorn);
+
+  function updateHorn (){
+    console.log('change was made');
+    images.src = "./assets/images/party-horn.svg";
+    
+  }
+  // Volume Slider
+
+  // Play sound
 }
+
